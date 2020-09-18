fd737b2798979e27354452662e42549c23e94645
ChivesFinance <71374284+ChivesFinance@users.noreply.github.com>
2020-09-18 13:16:39 +0200

    Update Deom  Chart 1.js

--- Deom  Chart 1.js
+++ Deom  Chart 1.js
@@ -24,13 +24,6 @@ am4core.ready(function() {
 
 /**
  * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
  * ---------------------------------------
  */
 
